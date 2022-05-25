cdc1cee4c3d497fdc68b209b07a0650e3db46be2
Pedro Martinelli <pedrovieiramartinelli@gmail.com>
2022-05-25 14:15:42 -0300

    Pawn

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -10,10 +10,10 @@ public class King extends ChessPiece {
 	public King(Board board, Color color) {
 		super(board, color);
 	}
-
+	
+	@Override
 	public String toString() {
 		return "K";
-
 	}
 
 	private boolean canMove(Position position) {
